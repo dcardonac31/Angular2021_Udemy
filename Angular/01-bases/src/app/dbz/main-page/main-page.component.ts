91670e928e590e6d76c191cf70da30bef8bc8a74
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-06-14 16:59:16 -0500

    Seccion 7: Desplegando en Netlify

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { Personaje } from '../interfaces/dbz.interface';
-import { DbzService } from '../services/dbz.service';
+
 
 @Component({
   selector: 'app-main-page',
